2baddb495403f0da28cfaf2aeae304d8e11157ff
joeymiels <>
2025-03-24 11:06:39 +0800

    Added array activity

---
+++ arrays/src/arrays/Arrays.java
@@ -0,0 +1,14 @@
+package arrays;
+
+public class Arrays {
+
+    public static void main(String[] args) {
+
+        int numbers[] = {1, 2, 3, 4, 5,};
+
+        int numbers = new [7];
+
+        System.out.println(numbers[3]);
+    }
+
+}
